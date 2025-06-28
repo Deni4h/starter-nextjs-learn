df0a5b542962fda82af719e8a9c58d6a77759eb6
Deni4h <denisatria4734@gmail.com>
2025-06-28 07:52:38 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! | new line</a>
+          Welcome to <a href="https://nextjs.org">Next.js! | new line | hey bro</a>
         </h1>
 
         <p className={styles.description}>
