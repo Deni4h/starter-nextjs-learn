74c2df3d57daff6f459148f68fff58ff4850e6a6
deni4h <denisatria4734@gmail.com>
2025-06-28 01:39:28 +0000

    firts

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! | new line | coba lagi | tsting dari branch = deni/testing</a>
+          Welcome to <a href="https://nextjs.org">Next.js! | new line | coba lagi | tsting dari branch = deni/testing | testing dari serv | env new</a>
            Stashed changes
         </h1>
 
